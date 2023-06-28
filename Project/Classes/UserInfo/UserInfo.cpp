bf4e6000701e7149c70460ba3ba816d9f9e59800
Mohammad Javad Samii <135453912+mjavadsamii@users.noreply.github.com>
2023-06-28 09:11:04 +0330

    deleting unnecessary files

--- Project/Classes/UserInfo/UserInfo.cpp
+++
@@ -1,8 +0,0 @@
-#include "UserInfo.h"
-
-UserInfo::UserInfo(std::string name, std::string family, int ID)
-    : name(name), family(family), ID(ID) {}
-
-std::string UserInfo::getName() {
-    return name;
-}
